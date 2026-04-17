368f12def92592ce3d14a6335b1bfbeb618880b1
Diego Gallardo <75384042+grdiego897@users.noreply.github.com>
2026-04-17 21:28:07 +0200

    Fix formatting and add newline at end of file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
